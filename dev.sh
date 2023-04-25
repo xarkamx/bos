01f4ce278c44021dbdc680824351dcaa5104b391
Alberto <xarkamx@gmail.com>
2023-04-25 00:13:28 -0600

    testing process

--- dev.sh
+++ dev.sh
@@ -1,5 +1,14 @@
 docker-compose up -d db
 sleep 5
+
+export DB_CONNECTION=mariadb
+export DB_HOST=localhost
+export DB_PORT=3306
+export DB_DATABASE=bos
+export DB_USERNAME=root
+export DB_PASSWORD=password
+
+
 yarn knex migrate:latest
 yarn dev
 docker-compose down
---
+++ test.sh
@@ -0,0 +1,6 @@
+# run local in the background
+yarn local &
+sleep 15
+yarn cucumber-js --require-module ts-node/register --require features/**/*.ts
+
+fkill -f :8000
